5fd453d4edff55eb5616df150ddca31f2a8f6cdd
Timo Bingmann <tbgit@panthema.net>
2014-03-29 18:53:36 +0100

    Adding Applet which embeds game in a java applet.

---
+++ src/net/panthema/BispanningGame/Applet.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * src/net/panthema/BispanningGame/Applet.java
+ *
+ * Run Game as a Java Applet.
+ *
+ *******************************************************************************
+ * Copyright (C) 2014 Timo Bingmann <tb@panthema.net>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+package net.panthema.BispanningGame;
+
+import javax.swing.SwingUtilities;
+
+public class Applet extends javax.swing.JApplet
+{
+    private static final long serialVersionUID = -7366375062835834817L;
+
+    public void init() {
+        try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                public void run() {
+                    add(new GamePanel());
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
